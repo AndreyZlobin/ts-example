650fc1af9f68a0d2ab018f2d63cd425e91146e42
AnreyZlobin <zlobin.andy@gmail.com>
2022-01-27 10:41:37 +0300

    Filters, Infer, notFalsy

--- index.ts
+++ index.ts
@@ -55,3 +55,46 @@ type Article = Omit<Book, "pagesCount">;
 type ArticleWithRequiredAuthor = Required<
   Pick<Book, "author"> & Omit<Book, "author">
 >;
+
+
+
+interface User {
+  id: string;
+}
+interface Message {
+  id: string;
+}
+
+
+/* function getId<T extends {id: any}>(obj: T): T extends User ? string : number{
+  return obj.id
+} */
+
+function getId<T extends {id: any}>(obj: T): T extends {id: string} ? string : number{
+  return obj.id
+}
+
+
+const r1 = getId({} as User) // string
+const r2 = getId({} as Message) // number
+
+
+type NotFalsy<T = null> = T extends (null | false | undefined | 0) ? never : T;
+
+// let z: NotFalsy  never
+
+// z = '' // error
+
+// let z: NotFalsy<string>  string
+
+// z = 'Holly Molly' // valid
+
+
+
+type Filter<T, U> = T extends U ? never : T;
+
+// type R = Filter<'a' | 'b' | 'c', 'b'>
+
+type TryInfer<T extends object = object> = T extends infer R ? R[keyof R] : never;
+
+// type R = TryInfer<{a: 0, b: 3}>
